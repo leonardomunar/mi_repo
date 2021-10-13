ffb1cce8152d0e7f742b0d80478630848b527fca
leonardomunar <kiramunar87@gmail.com>
2021-10-13 17:13:51 -0500

    mapeo de javascript

---
+++ JAVASCRIPT/mapeo.js
@@ -0,0 +1,35 @@
+{
+    ventas_facturacion = {
+        sede: String,
+        fecha: Date,
+        Hora: hour,
+        Numero_factura: Number,
+        Cliente: String,
+        productos: [],
+        subtotal: Number,
+        IVA: Number,
+        total: Number,
+    }
+
+    pedidos = {
+        Cliente: String,
+        direccion: Number,
+        telefono: Number,
+        Numero_factura: numero,
+        productos: [],
+
+    }
+
+    gastos = {
+        servicios: Number,
+        software: Number,
+        arriendo: Number,
+        plataformasDelivery: Number,
+        nomina: Number,
+    }
+
+    Compras[]
+}
+
+
+
